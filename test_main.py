b42ff6bdd8494b6c972de28ffb5e28c16e9ea86b
pedroflol <phaguia@outlook.com>
2022-11-20 19:32:54 -0300

    atutaliza2

--- test_main.py
+++ test_main.py
@@ -1,10 +1,9 @@
 from modulo_de_alteracao import *
 import unittest
 import sqlite3
-from sqlite3 import Error
 
 
-con = sqlite3.connect(r"C:\Users\Usuario\Grupo_3\supermarket.db") 
+con = sqlite3.connect("supermarket.db") 
 cursor = con.cursor()
 cursor.execute(f"""CREATE TABLE "Supermercado" ("Numero_do_produto"    INTEGER UNIQUE,"Nome_do_produto"    TEXT UNIQUE,"Quantidade_do_produto"    INTEGER,"Preco_do_produto"    NUMERIC,PRIMARY KEY("Numero_do_produto"));""") #insere as variáveis
 con.commit()
